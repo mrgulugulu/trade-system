93c670a71409aaa0b2924c826ff1a7101d698cc2
mrgulugulu <492238938@qq.com>
2023-02-04 12:04:28 +0800

    bugfix(main): 修复了main函数中关于5mink线终止时间的错误

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -31,7 +31,7 @@ func main() {
 		tradePair := model.TradePairWithTime{}
 		err = json.Unmarshal([]byte(tradePairStr), &tradePair)
 		if err != nil {
-			fmt.Printf("tradePair error: %v", err)
+			log.Printf("tradePair unmarshal error: %v", err)
 		}
 		t := tradePair.Time
 		// 这里考虑到有部分数据的交易时间并不是从0秒开始，所以单独处理掉，当做是分钟线
@@ -41,34 +41,19 @@ func main() {
 		} else if t%60 == 0 && len(tradePairIn1MinList) == 0 {
 			tEnd1Min = t + 60
 		}
-		if t%360 != 0 && flag5Min == 0 {
+		if t%300 != 0 && flag5Min == 0 {
 			flag5Min = 1
-			tEnd5Min = tradePair.Time + (360 - tradePair.Time)
-		} else if t%360 == 0 && len(tradePairIn5MinList) == 0 {
-			tEnd5Min = t + 360
+			tEnd5Min = tradePair.Time + (300 - tradePair.Time%300)
+		} else if t%300 == 0 && len(tradePairIn5MinList) == 0 {
+			tEnd5Min = t + 300
 		}
 		tradePairIn1MinList = append(tradePairIn1MinList, tradePair)
 		tradePairIn5MinList = append(tradePairIn5MinList, tradePair)
 
 		// 假定k线程序会一直运行，所以所有的数据都是以整分钟保存的
 		switch t {
-		case tEnd1Min:
-			kLineIn1Min := kline.KLineIn1MinGen(tradePairIn1MinList)
-			log.Printf("%v", kLineIn1Min)
-			err = d.SaveKLineInfo2Mysql(kLineIn1Min)
-			if err != nil {
-				log.Printf("save k line error: %v", err)
-			}
-			kByte, err := json.Marshal(kLineIn1Min)
-			if err != nil {
-				log.Printf("marshal error: %v", err)
-			}
-			d.Publish2Redis(config.KLineIn1MinChannelName, string(kByte))
-			// 重置交易对列表
-			tradePairIn1MinList = make([]model.TradePairWithTime, 0)
 		case tEnd5Min:
 			kLineIn5Min := kline.KLineIn5MinGen(tradePairIn1MinList)
-			log.Printf("%v", kLineIn5Min)
 			err = d.SaveKLineInfo2Mysql(kLineIn5Min)
 			if err != nil {
 				log.Printf("save k line error: %v", err)
@@ -80,6 +65,21 @@ func main() {
 			d.Publish2Redis(config.KLineIn5MinChannelName, string(kByte))
 			// 重置交易对列表
 			tradePairIn5MinList = make([]model.TradePairWithTime, 0)
+			fallthrough
+		case tEnd1Min:
+			kLineIn1Min := kline.KLineIn1MinGen(tradePairIn1MinList)
+			err = d.SaveKLineInfo2Mysql(kLineIn1Min)
+			if err != nil {
+				log.Printf("save k line error: %v", err)
+			}
+			kByte, err := json.Marshal(kLineIn1Min)
+			if err != nil {
+				log.Printf("marshal error: %v", err)
+			}
+			d.Publish2Redis(config.KLineIn1MinChannelName, string(kByte))
+			// 重置交易对列表
+			tradePairIn1MinList = make([]model.TradePairWithTime, 0)
+
 		}
 	}
 	// TODO: 需要一个优雅退出
--- internal/server/router.go
+++ internal/server/router.go
@@ -3,6 +3,7 @@ package server
 import (
 	"fmt"
 	"net/http"
+	"strconv"
 	"trade-system/internal/dao"
 	"trade-system/internal/model"
 
@@ -12,13 +13,20 @@ import (
 
 // queryKLineIn1Min 查询一分钟k线的信息
 func queryKLineIn1Min(c *gin.Context) {
+	queryAmountStr := c.DefaultQuery("amount", "10")
 	d, err := dao.NewDao()
 	if err != nil {
 		c.String(http.StatusInternalServerError, "databases connect errors: %v", d)
 		return
 	}
+	queryAmout, err := strconv.Atoi(queryAmountStr)
+	if err != nil {
+		c.String(http.StatusBadRequest, "query amount invalid: %v", err)
+		return
+	}
+
 	var kLineInfo []model.KLineIn1Min
-	res := d.MysqlDb.Order("time desc").Limit(10).Find(&kLineInfo)
+	res := d.MysqlDb.Order("time desc").Limit(queryAmout).Find(&kLineInfo)
 	switch res.Error {
 	case gorm.ErrInvalidDB:
 		c.String(http.StatusInternalServerError, "invalid db: %v", err)
@@ -34,13 +42,20 @@ func queryKLineIn1Min(c *gin.Context) {
 
 // queryKLineIn5Min 查询一分钟k线的信息
 func queryKLineIn5Min(c *gin.Context) {
+	queryAmountStr := c.DefaultQuery("amount", "10")
 	d, err := dao.NewDao()
 	if err != nil {
 		c.String(http.StatusInternalServerError, "databases connect errors: %v", d)
 		return
 	}
+	queryAmout, err := strconv.Atoi(queryAmountStr)
+	if err != nil {
+		c.String(http.StatusBadRequest, "query amount invalid: %v", err)
+		return
+	}
+
 	var kLineInfo []model.KLineIn5Min
-	res := d.MysqlDb.Order("time desc").Limit(10).Find(&kLineInfo)
+	res := d.MysqlDb.Order("time desc").Limit(queryAmout).Find(&kLineInfo)
 	switch res.Error {
 	case gorm.ErrInvalidDB:
 		c.String(http.StatusInternalServerError, "invalid db: %v", err)
