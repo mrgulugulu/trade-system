4033c7b3e636839c96339c22d85beb629372f0c4
haiming ye <492238938@qq.com>
2023-02-07 23:37:27 +0800

    perf: 调整了代码并加入了优雅关闭

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -16,11 +16,10 @@ func main() {
 		log.Sugar.Panicf("new dao error: %v", err)
 		return
 	}
-	s := server.Server{
+	s := &server.Server{
 		Addr: config.ServiceConf.ServerCfg.Addr,
 		Port: config.ServiceConf.ServerCfg.Port,
 	}
-	go s.Run()
 	tradePairChan := make(chan string)
 	go d.SubscribeFromRedis(config.TradePairChannelName, tradePairChan)
 	// TODO: 用切片的话，如果遇到巨量的交易对，可能会导致消耗过多的内存空间
@@ -31,63 +30,65 @@ func main() {
 	kLineIn1MinChan, kLineIn5MinChan := make(chan string, 10), make(chan string, 10)
 	go d.Publish2Redis(config.KLineIn5MinChannelName, kLineIn5MinChan)
 	go d.Publish2Redis(config.KLineIn1MinChannelName, kLineIn1MinChan)
-	for {
-		tradePairStr := <-tradePairChan
-		tradePair := model.TradePairWithTime{}
-		err = json.Unmarshal([]byte(tradePairStr), &tradePair)
-		if err != nil {
-			log.Sugar.Errorf("tradePair unmarshal error: %v", err)
-		}
-		t := tradePair.Time
-		// 这里考虑到有部分数据的交易时间并不是从0秒开始，所以单独处理掉，当做是分钟线
-		if t%60 != 0 && flag1Min == 0 {
-			flag1Min = 1
-			tEnd1Min = tradePair.Time + (60 - tradePair.Time%60)
-		} else if t%60 == 0 && len(tradePairIn1MinList) == 0 {
-			tEnd1Min = t + 60
-		}
-		if t%300 != 0 && flag5Min == 0 {
-			flag5Min = 1
-			tEnd5Min = tradePair.Time + (300 - tradePair.Time%300)
-		} else if t%300 == 0 && len(tradePairIn5MinList) == 0 {
-			tEnd5Min = t + 300
-		}
-		tradePairIn1MinList = append(tradePairIn1MinList, tradePair)
-		tradePairIn5MinList = append(tradePairIn5MinList, tradePair)
 
-		// 假定k线程序会一直运行，所以所有的数据都是以整分钟保存的
-		switch t {
-		case tEnd5Min:
-			kLineIn5Min := kline.KLineIn5MinGen(tradePairIn5MinList)
-			log.Sugar.Infof("kLineIn5Min: %+v", kLineIn5Min)
-			err = d.SaveKLineInfo2Mysql(kLineIn5Min)
+	go func() {
+		for {
+			tradePairStr := <-tradePairChan
+			tradePair := model.TradePairWithTime{}
+			err = json.Unmarshal([]byte(tradePairStr), &tradePair)
 			if err != nil {
-				log.Sugar.Errorf("save 5-min-k-line error: %v", err)
+				log.Sugar.Errorf("tradePair unmarshal error: %v", err)
 			}
-			kByte, err := json.Marshal(kLineIn5Min)
-			if err != nil {
-				log.Sugar.Errorf("5-min-k-line marshal error: %v", err)
+			t := tradePair.Time
+			// 这里考虑到有部分数据的交易时间并不是从0秒开始，所以单独处理掉，当做是分钟线
+			if t%60 != 0 && flag1Min == 0 {
+				flag1Min = 1
+				tEnd1Min = tradePair.Time + (60 - tradePair.Time%60)
+			} else if t%60 == 0 && len(tradePairIn1MinList) == 0 {
+				tEnd1Min = t + 60
 			}
-			kLineIn5MinChan <- string(kByte)
-			// 重置交易对列表
-			tradePairIn5MinList = make([]model.TradePairWithTime, 0)
-			fallthrough
-		case tEnd1Min:
-			kLineIn1Min := kline.KLineIn1MinGen(tradePairIn1MinList)
-			log.Sugar.Infof("kLineIn1Min: %+v", kLineIn1Min)
-			err = d.SaveKLineInfo2Mysql(kLineIn1Min)
-			if err != nil {
-				log.Sugar.Errorf("save 1-min-k-line error: %v", err)
+			if t%300 != 0 && flag5Min == 0 {
+				flag5Min = 1
+				tEnd5Min = tradePair.Time + (300 - tradePair.Time%300)
+			} else if t%300 == 0 && len(tradePairIn5MinList) == 0 {
+				tEnd5Min = t + 300
 			}
-			kByte, err := json.Marshal(kLineIn1Min)
-			if err != nil {
-				log.Sugar.Errorf("1-min-k-line marshal error: %v", err)
-			}
-			kLineIn1MinChan <- string(kByte)
-			// 重置交易对列表
-			tradePairIn1MinList = make([]model.TradePairWithTime, 0)
-		}
-	}
+			tradePairIn1MinList = append(tradePairIn1MinList, tradePair)
+			tradePairIn5MinList = append(tradePairIn5MinList, tradePair)
 
-	// TODO: 需要一个优雅退出
+			// 假定k线程序会一直运行，所以所有的数据都是以整分钟保存的
+			switch t {
+			case tEnd5Min:
+				kLineIn5Min := kline.KLineIn5MinGen(tradePairIn5MinList)
+				log.Sugar.Infof("kLineIn5Min: %+v", kLineIn5Min)
+				err = d.SaveKLineInfo2Mysql(kLineIn5Min)
+				if err != nil {
+					log.Sugar.Errorf("save 5-min-k-line error: %v", err)
+				}
+				kByte, err := json.Marshal(kLineIn5Min)
+				if err != nil {
+					log.Sugar.Errorf("5-min-k-line marshal error: %v", err)
+				}
+				kLineIn5MinChan <- string(kByte)
+				// 重置交易对列表
+				tradePairIn5MinList = make([]model.TradePairWithTime, 0)
+				fallthrough
+			case tEnd1Min:
+				kLineIn1Min := kline.KLineIn1MinGen(tradePairIn1MinList)
+				log.Sugar.Infof("kLineIn1Min: %+v", kLineIn1Min)
+				err = d.SaveKLineInfo2Mysql(kLineIn1Min)
+				if err != nil {
+					log.Sugar.Errorf("save 1-min-k-line error: %v", err)
+				}
+				kByte, err := json.Marshal(kLineIn1Min)
+				if err != nil {
+					log.Sugar.Errorf("1-min-k-line marshal error: %v", err)
+				}
+				kLineIn1MinChan <- string(kByte)
+				// 重置交易对列表
+				tradePairIn1MinList = make([]model.TradePairWithTime, 0)
+			}
+		}
+	}()
+	s.Run()
 }
--- internal/server/server.go
+++ internal/server/server.go
@@ -2,8 +2,13 @@
 package server
 
 import (
+	"context"
 	"fmt"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 	"trade-system/internal/log"
 
 	"github.com/gin-gonic/gin"
@@ -15,6 +20,7 @@ type Server struct {
 }
 
 func (s *Server) Run() {
+
 	r := gin.Default()
 	kLine1Min := r.Group("/kLine1Min")
 	{
@@ -33,35 +39,22 @@ func (s *Server) Run() {
 	}
 	// 搞个signal来监听，实现优雅关闭
 	log.Sugar.Infof("listening %v", ser.Addr)
-	ser.ListenAndServe()
-	// gracefulExitServer(ser)
+
+	go ser.ListenAndServe()
+	gracefulExitServer(ser)
 }
 
 // gracefulExitServer 服务器优雅退出
-// func gracefulExitServer(ser *http.Server) {
-// 	ch := make(chan os.Signal, 1)
-// 	signal.Notify(ch, syscall.SIGTERM, syscall.SIGQUIT, syscall.SIGINT)
-// 	<-ch
-// 	nowTime := time.Now()
-// 	movieTitleList, err := dao.D.GetMovieSetMembers(config.QueryMovieSet)
-// 	if err != nil {
-// 		log.Printf("get movie set member error: %v", err)
-// 	}
-// 	for _, title := range movieTitleList {
-// 		viewNum, err := dao.D.GetMovieViewNumber(title)
-// 		if err != nil {
-// 			log.Printf("get movie view number error: %v", err)
-// 		}
-// 		err = dao.D.UpdateMovieInfo(title, "view_number", viewNum)
-// 		if err != nil {
-// 			log.Printf("update movie viewNumber error: %v", err)
-// 		}
-// 	}
-// 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-// 	defer cancel()
-// 	err = ser.Shutdown(ctx)
-// 	if err != nil {
-// 		log.Printf("shutdown error: %v", err)
-// 	}
-// 	fmt.Println("-----exited-----", time.Since(nowTime))
-// }
+func gracefulExitServer(ser *http.Server) {
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, syscall.SIGTERM, syscall.SIGQUIT, syscall.SIGINT)
+	<-ch
+	nowTime := time.Now()
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	err := ser.Shutdown(ctx)
+	if err != nil {
+		log.Sugar.Errorf("shutdown error: %v", err)
+	}
+	log.Sugar.Info("-----exited-----", time.Since(nowTime))
+}
